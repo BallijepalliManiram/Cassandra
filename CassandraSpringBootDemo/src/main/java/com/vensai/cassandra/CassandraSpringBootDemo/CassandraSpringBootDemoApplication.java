43e90d0e562fae05e956e4a59a0188b145337805
maniram <balijepalli93@gmail.com>
2020-10-22 16:21:34 +0530

    committing changes

---
+++ CassandraSpringBootDemo/src/main/java/com/vensai/cassandra/CassandraSpringBootDemo/CassandraSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vensai.cassandra.CassandraSpringBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ CassandraSpringBootDemo/src/test/java/com/vensai/cassandra/CassandraSpringBootDemo/CassandraSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vensai.cassandra.CassandraSpringBootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandraSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
